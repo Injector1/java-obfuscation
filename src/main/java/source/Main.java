66082a751329995d79c6350f2f2e31fb61df3b8f
Demyan Kurbatov <kurbat02@ads.uni-passau.de>
2025-06-09 20:34:23 +0200

    Refactor Main.java to add method documentation and implement bubble sort algorithm

--- src/main/java/source/Main.java
+++ src/main/java/source/Main.java
@@ -9,10 +9,12 @@ public class Main {
         System.out.printf("Hello and welcome!");
 
         doSomething();
-        someArithmeticalOperations();
     }
 
-    private static void doSomething() {
+    /**
+     * This method demonstrates a simple loop.
+     */
+    public static void doSomething() {
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
@@ -20,30 +22,71 @@ public class Main {
         }
     }
 
-    private static int add(int a, int b) {
+    /**
+     * Addition method that takes two integers and returns their sum.
+     * @param a - the first integer
+     * @param b - the second integer
+     * @return the sum of a and b
+     */
+    public static int add(int a, int b) {
         return a + b;
     }
 
-    private static int subtract(int a, int b) {
+    /**
+     * Subtraction method that takes two integers and returns their difference.
+     * @param a - the first integer
+     * @param b - the second integer
+     * @return the difference of a and b
+     */
+    public static int subtract(int a, int b) {
         return a - b;
     }
 
-    private static int multiply(int a, int b) {
+    /**
+     * Multiplication method that takes two integers and returns their product.
+     * @param a - the first integer
+     * @param b - the second integer
+     * @return the product of a and b
+     */
+    public static int multiply(int a, int b) {
         return a * b;
     }
 
-    private static double divide(int a, int b) {
+    /**
+     * Division method that takes two integers and returns their quotient.
+     * Throws an ArithmeticException if the second integer is zero.
+     * @param a - the numerator
+     * @param b - the denominator
+     * @return the quotient of a and b
+     */
+    public static double divide(int a, int b) {
         if (b == 0) {
             throw new ArithmeticException("Division by zero");
         }
         return (double) a / b;
     }
 
-    private static void someArithmeticalOperations() {
-        System.out.println("Addition: " + add(10, 5));
-        System.out.println("Addition: " + add(12, 10));
-        System.out.println("Subtraction: " + subtract(10, 5));
-        System.out.println("Multiplication: " + multiply(10, 5));
-        System.out.println("Division: " + divide(10, 5));
+    /**
+     * Sorts an array of integers using the bubble sort algorithm.
+     *
+     * @param array - the array to be sorted
+     * @return the sorted array
+     */
+    public int[] bubbleSort(int[] array) {
+        int n = array.length;
+        boolean swapped;
+        do {
+            swapped = false;
+            for (int i = 0; i < n - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+            n--;
+        } while (swapped);
+        return array;
     }
 }
